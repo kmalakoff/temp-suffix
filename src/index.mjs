68c0e28ccb2e7d95ce653f894a790f215df0bf2a
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-10 18:09:17 -0800

    Upgrade modules

--- src/index.mjs
+++ src/index.mjs
@@ -1,27 +1,27 @@
 // Adapted from https://github.com/npm/write-file-atomic
 
-var MurmurHash3 = require('imurmurhash');
+import MurmurHash3 from 'imurmurhash';
 
 // if we run inside of a worker_thread, `process.pid` is not unique
 /* istanbul ignore next */
-var threadId = (function getId() {
+const threadId = (function getId() {
   try {
-    var workerThreads = require('worker_threads');
+    const workerThreads = require('worker_threads');
 
     /// if we are in main thread, this is set to `0`
     return workerThreads.threadId;
-  } catch (e) {
+  } catch (_e) {
     // worker_threads are not available, fallback to 0
     return 0;
   }
 })();
 
-var invocations = 0;
-module.exports = function tempSuffix(prefix) {
-  var suffix = MurmurHash3(prefix || '')
+let invocations = 0;
+export default function tempSuffix(prefix) {
+  const suffix = MurmurHash3(prefix || '')
     .hash(String(process.pid))
     .hash(String(threadId))
     .hash(String(++invocations))
     .result();
-  return prefix === undefined ? '' + suffix : prefix + '-' + suffix;
-};
+  return prefix === undefined ? `${suffix}` : `${prefix}-${suffix}`;
+}
